1cb0fe27a4aa23e1cd7f757e6b2c521d9f2c0049
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-02-11 16:19:22 -0600

    fixed deleted modles folder

---
+++ TestToDyeFor/Models/MXRecipe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestToDyeFor.Models
+{
+    public class MXRecipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DyeColor { get; set; }
+
+        public MXRecipe() { }
+
+        public MXRecipe(string name, string dyeColor)
+        {
+            Name = name;
+            DyeColor = dyeColor;
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MXRecipe recipe &&
+                   Id == recipe.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
