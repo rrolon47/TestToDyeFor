00823322c1b70e96cf757f81c30c1dc24ae3e535
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-02-11 16:17:10 -0600

    trying to connect to db

--- TestToDyeFor/Data/ApplicationDbContext.cs
+++ TestToDyeFor/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TestToDyeFor.Models;
 
 namespace TestToDyeFor.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext 
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
